9c3661e4da7dd190f67cbe013222ebf52c2401b9
AkshatSharma3124 <sharmaakshat5600@gmail.com>
2022-12-28 19:34:10 +0530

    Create Program5.c

---
+++ Program5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main() {
+    float i=0 ;
+    int t=0;
+    do{
+        printf("integer %d in float is %f\n",t,i);
+        t++;
+        i++;
+
+    }
+    while(t<=5,i<=5);
+    
+}
